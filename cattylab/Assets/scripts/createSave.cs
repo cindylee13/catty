1b3a39fbdbc3c36183ec2218a073b9e12c25b3cd
How Lemon <chickoutyo@gmail.com>
2018-05-04 02:43:13 +0800

    -Added different savefile types

---
+++ cattylab/Assets/scripts/createSave.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "playerSave",menuName ="My Asset/Create Player Save",order = 1)]
+public class createSave : ScriptableObject {
+
+    public int money;
+    public int maxGropuCount;
+    public int maxCats;
+    public int maxGroupPplCount;
+    public int unlockScore;
+    public gameEntities[] gameEntities;
+    public exploreGroups[] exploreGroups;
+
+}
+
+public struct gameEntities
+{
+    public bool isItem;
+    public int id;
+    public int count;
+}
+
+public struct exploreGroups
+{
+    public int[] crews;
+    public bool isOut;
+    public double backTime;
+    public int destination;
+}
---
+++ cattylab/Assets/scripts/playerStateControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerStateControl : MonoBehaviour {
+
+    private int money, maxGroupCount, maxCats, maxGroupPplCount;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
