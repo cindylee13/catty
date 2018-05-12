9103a872eb4dbf0538016800d6d35c66436e4cee
DESKTOP-SOJQJMF\maggi <maggie85.tw@gmail.com>
2018-05-12 17:40:53 +0800

    add new code in viewAutoScale.cs

--- cattylab/Assets/scripts/GUIAutoScale.cs
+++ cattylab/Assets/scripts/GUIAutoScale.cs
@@ -3,9 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GUIAutoScale : MonoBehaviour {
+    public float baseWidth = 411;
+    public float baseHeight = 731;
 
-	// Use this for initialization
-	void Start () {
+    private float baseAspect;   //開發時定義的基礎解析度長寬比
+    private float targetAspect; //實際顯示畫面的長寬比
+    private float m03; //實際畫面上，GUI的x軸起始點為從左邊算來第幾個像素(pixel)
+    private float m13; //實際畫面上，GUI的y軸起始點為從上方算來第幾個像素(pixel)
+    private float m33; //x軸及 y軸等比縮放比例的反比，也就是說，數值變大為等比縮小，數值變小則等比
+    
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
--- cattylab/Assets/scripts/viewAutoScale.cs
+++ cattylab/Assets/scripts/viewAutoScale.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class viewAutoScale : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public float baseWidth = 411;
+    public float baseHeight = 731;
+    public float baseOrthographicSize = 5; //檢視空間的一半大小
+    public Camera camera;
+
+    // Use this for initialization
+    void Start () {
+        float newOrthographicSize = (float)Screen.height / (float)Screen.width * this.baseWidth / this.baseHeight * this.baseOrthographicSize;
+        camera.orthographicSize = Mathf.Max(newOrthographicSize, this.baseOrthographicSize);
+    }
 	
 	// Update is called once per frame
 	void Update () {
