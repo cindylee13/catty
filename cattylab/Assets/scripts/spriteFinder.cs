e68fe75b1a9d4eaf35d3ab07c46e7e1cc7b8e7d1
MSI\HowLemon <chickoutyo@gmail.com>
2018-05-14 22:58:29 +0800

    -WIP Asset Finder

---
+++ cattylab/Assets/scripts/spriteFinder.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spriteFinder : MonoBehaviour {
+
+	public Sprite _notFound;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public Sprite findSpriteByEntityID(int id, string type){
+		Sprite foundSprite;
+		string path = "";
+		switch(type){
+			case "cat":
+			path = string.Format("Entities/c_{0}", id);
+			break;
+			case "item":
+			path = string.Format("Entities/i_{0}", id);
+			break;
+		}
+		
+		foundSprite = Resources.Load<Sprite>(path);
+		Debug.Log(foundSprite);
+		if(foundSprite == null) return _notFound;
+		else return foundSprite;
+	}
+}
---
+++ cattylab/Assets/scripts/walkingCats.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class walkingCats : MonoBehaviour {
+
+	public spriteFinder _spriteFinder;
+	public int catID;
+	private SpriteRenderer sr;
+
+	// Use this for initialization
+	void Start () {
+		sr = GetComponent<SpriteRenderer>();
+		sr.sprite = _spriteFinder.findSpriteByEntityID(catID, "cat");	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
