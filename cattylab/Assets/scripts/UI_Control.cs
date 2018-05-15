c065a5f71093055b3a31f6b6cbcfda055aa7ce6a
MSI\HowLemon <chickoutyo@gmail.com>
2018-05-16 03:42:06 +0800

    -WIP UIcontrol

---
+++ cattylab/Assets/scripts/UI_Control.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Control : MonoBehaviour {
+	
+	public cattyLabDictionaty CLD;
+	public List<Text> _moneyText;
+	public List<Text> _exploreTimeText;
+	public MainUI_GroupCounterControl _GCC;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
