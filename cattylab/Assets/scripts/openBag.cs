5fe357288b9ea2e02f95e8a9ea8016bb9a5d3baf
a104590019 <andylaw10108@gmail.com>
2018-05-16 17:53:22 +0800

    item bag V1

--- cattylab/Assets/scripts/openBag.cs
+++ cattylab/Assets/scripts/openBag.cs
@@ -10,13 +10,16 @@ public class openBag : MonoBehaviour {
     int _openOrClose = 2;
     public double y = 0;
     public double speed = 0.5;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    public playerStateControl overallStats;
+    public GameObject cat;
+    // Use this for initialization
+    void Start () {
+        overallStats.CatControl(1, 1, CatControlType.count);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
 		if(_openOrClose == 1 && y != 0)
         {
             _craftingList.transform.position += new Vector3(0, (float)-speed, 0);
@@ -30,7 +33,10 @@ public class openBag : MonoBehaviour {
     }
     void OnSelected()
     {
-        if(_openOrClose == 0)
+        cat = Instantiate(this.cat);
+        cat.transform.parent = _craftingList.transform;
+        cat.transform.localPosition =new Vector3(-4,-8,-5);
+        if (_openOrClose == 0 || _openOrClose == 2)
         {
             y = _craftingList.transform.position.y - 10.5;
             _openOrClose = 1;
